30053c8441d253a4a2aeb40179d1d19913bcad86
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2022-10-16 14:15:32 -0300

    Incluindo formatação de data

--- projetoUsuario/src/main/java/com/antony/projetousuario/ProjetoUsuarioApplication.java
+++ projetoUsuario/src/main/java/com/antony/projetousuario/ProjetoUsuarioApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -21,6 +24,11 @@ public class ProjetoUsuarioApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+        System.out.println(LocalDateTime.parse("25/03/2022 10:40:50",formater));
+
         Usuario usuario = new Usuario(null, "Antony", "Antony1234","Arkham90@");
         Usuario usuarioDois = new Usuario(null, "DEVELOPER", "DEVELOPER12","DEVbooT77");
 
