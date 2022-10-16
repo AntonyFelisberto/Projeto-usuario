a2d1f79d9c21f0e2f69a9fe597961b4ceb106f2d
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2022-10-16 12:18:33 -0300

    Implementando repository Incluindo metodo para inclusão de usuario Separando propriedades do properties

--- projetoUsuario/src/main/java/com/antony/projetousuario/ProjetoUsuarioApplication.java
+++ projetoUsuario/src/main/java/com/antony/projetousuario/ProjetoUsuarioApplication.java
@@ -1,13 +1,29 @@
 package com.antony.projetousuario;
 
+import com.antony.projetousuario.model.Usuario;
+import com.antony.projetousuario.repository.UsuarioRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ProjetoUsuarioApplication {
+@AllArgsConstructor
+public class ProjetoUsuarioApplication implements CommandLineRunner {
+
+    private UsuarioRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetoUsuarioApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Usuario usuario = new Usuario(null, "Antony", "Antony1234","Arkham90@");
+        Usuario usuarioDois = new Usuario(null, "DEVELOPER", "DEVELOPER12","DEVbooT77");
+
+        repository.saveAll(Arrays.asList(usuario, usuarioDois));
+    }
 }
