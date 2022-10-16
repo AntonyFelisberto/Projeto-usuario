c8cc95ee54253bb9c0f547673094f4dc483d622c
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2022-10-16 19:05:17 -0300

    adicionando mudança

--- projetoUsuario/src/main/java/com/antony/projetousuario/handler/ControllerExceptionHandler.java
+++ projetoUsuario/src/main/java/com/antony/projetousuario/handler/ControllerExceptionHandler.java
@@ -7,13 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletRequest;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(ObjectsException.class)
-    public ResponseEntity<StandardError> objectError(ObjectsException object, HttpServletRequest request){
+    public ResponseEntity<StandardError> objectError(ObjectsException object, ServletRequest request){
         StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), System.currentTimeMillis(), object.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
